9fbc3db43e90299617dff8f329bb2a35c9f554f6
Brandon Kobel <brandon@collabramusic.com>
2015-08-10 08:15:38 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = function importer(url, prev, done){
+  var regex = /^~/;
+  if (!url.match(regex)) {
+
+    var cssImportRegex = /^((\/\/)|(http:\/\/)|(https:\/\/))/;
+    // if we don't escape this, then it's breaking the normal css @import
+    if (url.match(cssImportRegex)) {
+      return done({file: '\'' + url + '\''});
+    }
+
+    return done({file: url});
+  }
+
+  var newFile = path.join(__dirname, 'node_modules', url.replace(regex, ''));
+  return done({file: newFile});
+}
